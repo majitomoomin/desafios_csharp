a5e7f1d3f93d7fbd2e4bbfe028b6e4180c945721
majitomoomin <mariajoseportill2@gmail.com>
2024-02-29 11:44:27 -0600

    #cambio 1, Agregar archivos de proyecto.

---
+++ desafio3/desafio3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace desafio3
+{
+    class sumadevalores
+    {
+        static void Main(String[] args)
+        {
+            try
+            {
+                Console.WriteLine("Ingrese el primer valor: ");
+                int valor1 = int.Parse(Console.ReadLine());
+                Console.WriteLine("Ingrese el segundp número: ");
+                int valor2 = int.Parse(Console.ReadLine());
+                int suma = valor1 + valor2;
+                Console.WriteLine($"La Suma de los valores es: {suma}");
+            }
+            catch(FormatException)
+            {
+                Console.Error.WriteLine("Error: debes ingresar un número valido");
+            }
+            catch (Exception e)
+            {
+                Console.Error.WriteLine($"Error inesperado: {e.Message}");
+            }
+        }
+
+    }
+}
